3630053c176e4ccdc38e9fa00c4f20e395b07062
Vince Campanale <vcampanale0294@gmail.com>
2024-05-15 15:35:29 -0700

    Export course hole distances object via index

--- src/index.js
+++ src/index.js
@@ -11,12 +11,14 @@ const {
   COURSE_MODE_PARS,
 } = require('./course-constants');
 const strokesGainedValues = require('./strokes-gained-values');
+const courseHoleDistances = require('./course-hole-distances');
 
 module.exports = {
   COURSES,
   COURSE_HOLE_PARS,
   COURSE_MODE_PARS,
   strokesGainedValues,
+  courseHoleDistances,
   convertLieType,
   calculateEffectiveDistance,
   calculateEffectiveDistanceIndex,
