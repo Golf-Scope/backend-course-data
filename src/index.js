46b996edc99df7c2bf5545f81a600bbbcf65fd4f
Vince Campanale <vcampanale0294@gmail.com>
2024-06-05 10:14:29 -0700

    Use coordinates to calculate distances

--- src/index.js
+++ src/index.js
@@ -11,13 +11,46 @@ const {
   COURSE_MODE_PARS,
 } = require('./course-constants');
 const strokesGainedValues = require('./strokes-gained-values');
-const courseHoleDistances = require('./course-hole-distances');
+const courseHoleCoordinates = require('./course-hole-coordinates');
 
-const getHoleDistanceTeeToPin = ({ course, hole, tee, pin }) => {
+const getHorizontalDistanceFromCoordinates = (x1, x2, z1, z2) => {
+  return Math.round(1000 * Math.sqrt((x2 - x1) ** 2 + (z2 - z1) ** 2)) / 1000;
+};
+
+const getTotalDistanceFromCoordinates = (x1, x2, y1, y2, z1, z2) => {
+  return (
+    Math.round(
+      1000 * Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2 + (z2 - z1) ** 2)
+    ) / 1000
+  );
+};
+
+const getTeeCoordinates = ({ course, hole, tee }) => {
   if (!['front', 'middle', 'back'].includes(tee)) {
     throw new Error('Tee must be one of front, middle, back');
   }
 
+  if (
+    ![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18].includes(
+      hole
+    )
+  ) {
+    throw new Error('Hole must be integer between 1 and 18');
+  }
+
+  const teeCoordinates =
+    courseHoleCoordinates[course][hole - 1].teeCoordinates[tee];
+
+  if (!teeCoordinates) {
+    throw new Error(
+      `No tee coordinates found for ${course}, hole ${hole}, ${tee} tee`
+    );
+  }
+
+  return teeCoordinates;
+};
+
+const getPinCoordinates = ({ course, hole, pin }) => {
   if (!['easy', 'medium', 'hard'].includes(pin)) {
     throw new Error('Pin must be one of easy, medium, hard');
   }
@@ -30,15 +63,26 @@ const getHoleDistanceTeeToPin = ({ course, hole, tee, pin }) => {
     throw new Error('Hole must be integer between 1 and 18');
   }
 
-  const holeDistance = courseHoleDistances[course][hole - 1][`${tee}_${pin}`];
+  const pinCoordinates =
+    courseHoleCoordinates[course][hole - 1].pinCoordinates[pin];
 
-  if (holeDistance == null) {
+  if (!pinCoordinates) {
     throw new Error(
-      `No hole distance found for ${course} hole ${hole} ${tee} ${pin}`
+      `No pin coordinates found for ${course}, hole ${hole}, ${pin} pin`
     );
   }
 
-  return holeDistance;
+  return pinCoordinates;
+};
+
+const getHoleDistanceTeeToPin = ({ course, hole, tee, pin }) => {
+  const teeCoordinates = getTeeCoordinates({ course, hole, tee });
+  const pinCoordinates = getPinCoordinates({ course, hole, pin });
+
+  const [x1, z1] = [teeCoordinates[0], teeCoordinates[2]];
+  const [x2, z2] = [pinCoordinates[0], pinCoordinates[2]];
+
+  return getHorizontalDistanceFromCoordinates(x1, x2, z1, z2);
 };
 
 module.exports = {
@@ -52,4 +96,7 @@ module.exports = {
   calculateStrokesGainedForRound,
   calculateStrokesGainedForShot,
   getHoleDistanceTeeToPin,
+  getTeeCoordinates,
+  getHorizontalDistanceFromCoordinates,
+  getTotalDistanceFromCoordinates,
 };
